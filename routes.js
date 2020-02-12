22f6db5e7336638b50d4722a31373646967351f0
Carmen <carmensantanadaw@gmail.com>
2020-02-12 09:23:50 +0100

    New routes

---
+++ routes.js
@@ -0,0 +1,49 @@
+const { User, Product, Cart, Purchase } = require("./models.js");
+const express = require("express");
+const router = express.Router();
+
+// READ ALL PRODUCTS
+router.get("/products", (req, res) => {
+    Product.find({}, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+});
+// READ ONE PRODUCT
+router.get("/products/:id", (req, res) => {
+    Product.findOne({ _id: req.params.id }, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+});
+
+// DELETE ONE PRODUCT
+router.delete("/products/:id", (req, res) => {
+    Product.findOneAndRemove({ _id: req.params.id }, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+});
+
+// UPDATE ONE PRODUCT
+router.put("/products/:id",  (req, res) => {
+    Product.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: { title: req.body.title, price: req.body.price, description: req.body.description, stock: req.body.stock, image: req.body.image } }, 
+        (err, data) => {
+            if (err) res.json({ error: err });
+            else res.json(data);
+        }
+    );
+});
+
+// POST/CREATE ONE PRODUCT
+router.post("/products", (req, res) => {
+    const Product = new Product({ title: req.body.title, price: req.body.price, description: req.body.description, stock: req.body.stock, image: req.body.image });
+    Product.save((err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+});
+
+module.exports = router;
