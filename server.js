4288f1c67d6fe1a2cb881d4266a9820b48b38e48
Kibrands <jjextreme13@gmail.com>
2020-02-13 08:43:00 +0100

    env variables added

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
+const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const apiRoutes = require('./routes.js');
 
-mongoose.connect("mongodb+srv://julio:julio@cluster0-pdbce.mongodb.net/devpc?retryWrites=true&w=majority", { useNewUrlParser: true })
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(DB_URI, { useNewUrlParser: true })
     .then(db => console.log("DB correctly connected"))
     .catch(error => console.log("Error connecting DB: " + error));
 
-const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 3000, () => console.log("Server initialized..."));
+app.listen(PORT, () => console.log("Server initialized..."));
