5fba0459502ced125702c1955e48b938bd65dc3b
Gloria <you@example.com>
2020-02-13 09:04:50 +0100

    routes & controlers modified (users aded)

--- controllers.js
+++ controllers.js
@@ -1,7 +1,108 @@
 const { User, Product, Cart, Purchase } = require("./models.js");
 
+//PRODUCTOS
 exports.readProducts = (req, res) => {
     Product.find({}, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+exports.readProduct = (req, res) => {
+    Product.findOne({ _id: req.params.id }, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+exports.deleteProduct = (req, res) => {
+    Product.findOneAndRemove({ _id: req.params.id }, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+exports.putProduct = (req, res) => {
+    Product.findOneAndUpdate({ _id: req.params.id }, {
+        $set: {
+            title: req.body.title,
+            price: req.body.price,
+            description: req.body.description,
+            stock: req.body.stock,
+            image: req.body.image,
+            dto: req.body.dto
+        }
+    },
+        (err, data) => {
+            if (err) res.json({ error: err });
+            else res.json(data);
+        }
+    );
+}
+
+exports.postProduct = (req, res) => {
+    const product = new Product({
+        title: req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        stock: req.body.stock,
+        image: req.body.image,
+        dto: req.body.dto
+    });
+    product.save((err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+//USUARIOS
+exports.readUsers = (req, res) => {
+    User.find({}, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+exports.readUser = (req, res) => {
+    User.findOne({ _id: req.params.id }, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+exports.deleteUser = (req, res) => {
+    User.findOneAndRemove({ _id: req.params.id }, (err, data) => {
+        if (err) res.json({ error: err });
+        else res.json(data);
+    });
+}
+
+exports.putUser = (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.id }, {
+        $set: {
+            title: req.body.title,
+            price: req.body.price,
+            description: req.body.description,
+            stock: req.body.stock,
+            image: req.body.image
+        }
+    },
+        (err, data) => {
+            if (err) res.json({ error: err });
+            else res.json(data);
+        }
+    );
+}
+
+exports.postUser = (req, res) => {
+    const user = new User({
+        title: req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        stock: req.body.stock,
+        image: req.body.image
+    });
+    user.save((err, data) => {
         if (err) res.json({ error: err });
         else res.json(data);
     });
--- routes.js
+++ routes.js
@@ -7,111 +7,30 @@ const router = express.Router();
 router.get("/products", cors(), controller.readProducts);
 
 // READ ONE PRODUCT
-router.get("/products/:id", (req, res) => {
-    Product.findOne({ _id: req.params.id }, (err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+router.get("/products/:id", cors(), controller.readProduct);
 
 // DELETE ONE PRODUCT
-router.delete("/products/:id", (req, res) => {
-    Product.findOneAndRemove({ _id: req.params.id }, (err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+router.delete("/products/:id", cors(), controller.deleteProduct);
 
 // UPDATE ONE PRODUCT
-router.put("/products/:id", (req, res) => {
-    Product.findOneAndUpdate({ _id: req.params.id }, {
-            $set: {
-                title: req.body.title,
-                price: req.body.price,
-                description: req.body.description,
-                stock: req.body.stock,
-                image: req.body.image,
-                dto: req.body.dto
-            }
-        },
-        (err, data) => {
-            if (err) res.json({ error: err });
-            else res.json(data);
-        }
-    );
-});
+router.put("/products/:id", cors(), controller.putProduct);
 
-// POST/CREATE ONE PRODUCT
-router.post("/products", (req, res) => {
-    const product = new Product({
-        title: req.body.title,
-        price: req.body.price,
-        description: req.body.description,
-        stock: req.body.stock,
-        image: req.body.image,
-        dto: req.body.dto
-    });
-    product.save((err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+// CREATE ONE PRODUCT
+router.post("/products", cors(), controller.postProduct);
 
 // READ ALL USERS
-router.get("/users", (req, res) => {
-    User.find({}, (err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+router.get("/users", cors(), controller.readUsers);
 
 // READ ONE USER
-router.get("/users/:id", (req, res) => {
-    User.findOne({ _id: req.params.id }, (err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+router.get("/users/:id", cors(), controller.readUser);
 
 // DELETE ONE USER
-router.delete("/users/:id", (req, res) => {
-    User.findOneAndRemove({ _id: req.params.id }, (err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+router.delete("/users/:id", cors(), controller.deleteUser);
 
 // UPDATE ONE USER
-router.put("/users/:id", (req, res) => {
-    User.findOneAndUpdate({ _id: req.params.id }, {
-            $set: {
-                title: req.body.title,
-                price: req.body.price,
-                description: req.body.description,
-                stock: req.body.stock,
-                image: req.body.image
-            }
-        },
-        (err, data) => {
-            if (err) res.json({ error: err });
-            else res.json(data);
-        }
-    );
-});
+router.put("/users/:id", cors(), controller.putUser);
 
-// POST/CREATE ONE USER
-router.post("/users", (req, res) => {
-    const user = new User({
-        title: req.body.title,
-        price: req.body.price,
-        description: req.body.description,
-        stock: req.body.stock,
-        image: req.body.image
-    });
-    user.save((err, data) => {
-        if (err) res.json({ error: err });
-        else res.json(data);
-    });
-});
+// CREATE ONE USER
+router.post("/users", cors(), controller.postUser);
 
 module.exports = router;
